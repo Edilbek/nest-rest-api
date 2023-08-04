3ea7b5dcdbab119bdd6d4227ed12ab1b62484672
Edilbek <edil.talantbekov@gmail.com>
2023-08-04 19:29:46 +0600

    move MongoDbUri into .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-const uri =
-  'mongodb+srv://ediltalantbekov:parol123@cluster0.858etxj.mongodb.net/?retryWrites=true&w=majority';
 
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot(uri)],
+  imports: [
+    ConfigModule.forRoot(),
+    ProductsModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DB_URI'), // Loaded from .ENV
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
