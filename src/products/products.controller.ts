458d14afaa8cfa4fc23f4d190aed400644898d0b
Edilbek <edil.talantbekov@gmail.com>
2023-08-04 18:14:15 +0600

    put commented examples

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,16 +6,31 @@ import {
   Put,
   Param,
   Body,
+  // Redirect,
+  HttpCode,
+  HttpStatus,
+  Header,
+  // Req,
+  // Res,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+// import { Request, Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
   @Get()
+  // @Redirect('https://google.com', 301)
   getAll() {
     return 'getAll';
   }
+
+  // @Get()
+  // // @Redirect('https://google.com', 301)
+  // getAll(@Req req: Request, @Res res: Response) {
+  //   res.status(201).end('Bye');
+  //   return 'getAll';
+  // }
 
   @Get(':id')
   getOne(@Param('id') id: string): string {
@@ -23,6 +38,8 @@ export class ProductsController {
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Cache-Control', 'none')
   create(@Body() createProductDto: CreateProductDto): string {
     return `Title: ${createProductDto.title}, Price: ${createProductDto.price}`;
   }
