509eeb3079a3299afe0bef17e6d3f8a4b3102e32
Edilbek <edil.talantbekov@gmail.com>
2023-08-04 19:10:20 +0600

    add models and connect mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+const uri =
+  'mongodb+srv://ediltalantbekov:parol123@cluster0.858etxj.mongodb.net/?retryWrites=true&w=majority';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot(uri)],
   controllers: [AppController],
   providers: [AppService],
 })
