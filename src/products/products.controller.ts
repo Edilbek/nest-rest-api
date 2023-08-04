52c6b65b0e0e8279bc88ae85d44abfc54c902401
Edilbek <edil.talantbekov@gmail.com>
2023-08-04 18:02:35 +0600

    add DTOs and update product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get()
+  getAll() {
+    return 'getAll';
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): string {
+    return 'getOne ' + id;
+  }
+
+  @Post()
+  create(@Body() CreateProductDto: CreateProductDto): string {
+    return `Title: ${CreateProductDto.title}, Price: ${CreateProductDto.price}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return `Removed ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
+    return `Updated ${id}`;
+  }
+}
